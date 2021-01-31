7f6f5c3ec0cad4bd743875e008bf316a0cc7769a
Paul Hewlett <paulhewlett@gmail.com>
2021-01-31 15:24:32 +0000

    New base image

--- buildscripts/tools.sh
+++ buildscripts/tools.sh
@@ -2,19 +2,13 @@
 # 
 # install additional tools
 # 
+set -x
 . ./buildscripts/_run_in_container_if_necessary
 
 # install tools
 cd source
-go get -u \
-    github.com/golang/protobuf/protoc-gen-go@v${PROTOBUF_VERSION}  \
-    golang.org/x/tools/cmd/goimports
-
-#GRPC_GATEWAY_VERSION=1.11.1
-#go get \
-#	github.com/grpc-ecosystem/grpc-gateway@v${GRPC_GATEWAY_VERSION} \
-#	github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@v${GRPC_GATEWAY_VERSION} \
-#	github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger@v${GRPC_GATEWAY_VERSION} \
+go mod tidy -v
+go mod verify
 
 # Fill cache
 go mod download
--- tools/tools.go
+++ tools/tools.go
@@ -8,14 +8,20 @@
 package tools
 
 import (
+	_ "github.com/AlekSi/gocov-xml"
+	_ "github.com/axw/gocov/gocov"
 	_ "github.com/envoyproxy/protoc-gen-validate"
 	_ "github.com/fullstorydev/grpcurl/cmd/grpcurl"
 	_ "github.com/go-delve/delve/cmd/dlv"
+	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/matm/gocov-html"
 	_ "github.com/mitchellh/golicense"
 	_ "github.com/jondot/goweight"
 	_ "github.com/leighmcculloch/gobin"
-	_ "github.com/rakyll/statik"
+	_ "github.com/psampaz/go-mod-outdated"
+	_ "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc"
 	_ "github.com/spf13/cobra"
 	_ "golang.org/x/tools/cmd/goimports"
+	_ "gotest.tools/v3/assert/cmd/gty-migrate-from-testify"
 	_ "rsc.io/goversion"
 )
